661c493b4c13952ac0cadba364a8f17ef8964eeb
Yekazad Software Center <91857222+cigerxwinislam@users.noreply.github.com>
2024-12-19 05:19:59 +0300

    Update page.tsx

--- src/app/(main)/bookmarks/page.tsx
+++ src/app/(main)/bookmarks/page.tsx
@@ -1,3 +1,7 @@
+// Bismillahirahmanirahim 
+
+
+
 import TrendsSidebar from "@/components/TrendsSidebar";
 import { Metadata } from "next";
 import Bookmarks from "./Bookmarks";
@@ -11,7 +15,7 @@ export default function Page() {
     <main className="flex w-full min-w-0 gap-5">
       <div className="w-full min-w-0 space-y-5">
         <div className="rounded-2xl bg-card p-5 shadow-sm">
-          <h1 className="text-center text-2xl font-bold">Bookmarks</h1>
+          <h1 className="text-center text-2xl font-bold">Bêrika We</h1>
         </div>
         <Bookmarks />
       </div>
