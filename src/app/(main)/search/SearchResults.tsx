df430eaa1e8666a7cd3554f6ee714a2127d3d63c
mmrxistin <mmrxistin@gmail.com>
2025-11-23 22:55:48 +0300

    SuphanAllah ve Bihamdihi velhamdulillah

--- src/app/(main)/search/SearchResults.tsx
+++ src/app/(main)/search/SearchResults.tsx
@@ -17,7 +17,7 @@
 import InfiniteScrollContainer from "@/components/InfiniteScrollContainer";
 import Post from "@/components/posts/Post";
 import PostsLoadingSkeleton from "@/components/posts/PostsLoadingSkeleton";
-import kyInstance from "@/lib/ky";
+import kyInstance from "@/lib";
 import { PostsPage } from "@/lib/types";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import { Loader2 } from "lucide-react";
